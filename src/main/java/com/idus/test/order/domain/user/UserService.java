b0e72d8f729fa43055726e66cdb113d1bbfa3f78
pointehd <pointehd@naver.com>
2022-05-16 23:47:03 +0900

    코드 누락 내용 추가
    회원가입 서비스 수정

--- src/main/java/com/idus/test/order/domain/user/UserService.java
+++ src/main/java/com/idus/test/order/domain/user/UserService.java
@@ -5,6 +5,7 @@ import com.idus.test.order.controller.dto.UserDto;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,14 +15,33 @@ import java.util.Optional;
 @Service
 public class UserService {
     UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
     public Optional<UserDto.UserInfo> findById(String id) {
         return userRepository.findByUserName(id)
                 .map(UserDto.UserInfo::from);
     }
 
-    // TODO 미구현 상태  구현 필요
     public boolean matchPassword(Token.Request request) {
-        return false;
+        return userRepository.findByUserName(request.getId())
+                .map(UserEntity::getPassword)
+                .map(password-> passwordEncoder.matches(request.getSecret(), password))
+                .orElse(false);
+    }
+
+    public boolean singUser(UserDto.SignupReqeust request) {
+        userNameMatchThrowError(request.getUserName());
+        request.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(UserEntity.from(request));
+        return true;
+    }
+
+    public void userNameMatchThrowError(String userName) {
+        if(userRepository.findByUserName(userName).isPresent()){
+            throw new IllegalArgumentException("아이디 중복입니다.");
+        }
+
+
+
     }
 }
