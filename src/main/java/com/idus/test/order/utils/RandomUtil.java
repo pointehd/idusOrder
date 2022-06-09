c6bd9ca8ff7d0db76c4b321c2ffb5a3d412f4d9a
pointehd <pointehd@naver.com>
2022-06-09 21:52:46 +0900

    Random seed 값 수정
    Random seed 값, Random 에대 해 공부 필요

--- src/main/java/com/idus/test/order/utils/RandomUtil.java
+++ src/main/java/com/idus/test/order/utils/RandomUtil.java
@@ -4,10 +4,9 @@ import java.util.Random;
 
 public class RandomUtil {
     private static final String MIX = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    private static final Random random = new Random();
+    private static final Random random = new Random(System.currentTimeMillis());
 
     public static String randomCode(int length) {
-        random.setSeed(System.currentTimeMillis());
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < length; i++) {
             sb.append(MIX.charAt(random.nextInt(36)));
